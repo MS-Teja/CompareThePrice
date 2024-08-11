aa9133c795dd1b34be1775f67be937f18be95429
MS-Teja <sivatejamutyala@gmail.com>
2024-08-11 11:43:08 +0530

    Initial Commit

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
